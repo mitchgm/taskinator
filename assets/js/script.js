c7daf722cb9633301944e569ada94621b84f426f
mitchgm <mitchgm@bgsu.edu>
2022-06-03 19:55:18 -0700

    added delete button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function(event) {
   event.preventDefault();
@@ -106,6 +107,26 @@ for (var i = 0; i < statusChoices.length; i++){
 
 formEl.addEventListener("submit", taskFormHandler);
 
+var taskButtonHandler = function(event) {
+  console.log(event.target);
+
+  if (event.target.matches(".delete-btn")) {
+    // get the element's task id
+
+    var taskId = event.target.getAttribute("data-task-id");
+    deleteTask(taskId);
+  }
+};
+
+var deleteTask= function(taskId) {
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+  taskSelected.remove();
+};
+
+
+
+
+pageContentEl.addEventListener("click", taskButtonHandler);
 
 
 
